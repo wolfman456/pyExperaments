0ad830271c9e8ca632fc4987ad1556be220e3e0a
wolfman456 <pshook456@gmail.com>
2024-03-23 10:59:06 -0500

    load screen started

--- spaceshooter/Util/gameobject.py
+++ spaceshooter/Util/gameobject.py
@@ -6,11 +6,18 @@ class ShooterObject:
         self._init_pygame()
         self.screen = pygame.display.set_mode((800, 600))
 
-    def load_screen(self):
-        pass
+    def _load_screen(self):
+        print("here")
+        font = pygame.font.Font(None, 36)
+        welcome = font.render("Welcome", True, "white")
+        self.screen.blit(welcome, (self.screen.get_width() / 2, self.screen.get_height() / 2))
+        pygame.display.flip()
 
     def main_loop(self):
+
         while True:
+            self._load_screen()
+
             self._handle_input()
 
             self._process_game_logic()
@@ -26,12 +33,15 @@ class ShooterObject:
         pygame.display.set_caption('Space shooter')
 
     def _handle_input(self):
-        pass
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or (
+                    event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+                quit()
 
     def _process_game_logic(self):
         pass
 
 
-# if __name__ == '__main__':
-#     shooter = ShooterObject()
-#     shooter.main_loop()
+if __name__ == '__main__':
+    shooter = ShooterObject()
+    shooter.main_loop()
